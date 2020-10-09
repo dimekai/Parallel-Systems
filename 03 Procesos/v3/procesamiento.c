42fd811a5badbc38a60e0e580835244ff665981b
JesusDiaz08 <jesus_kdm@hotmail.com>
2020-10-09 15:07:58 -0500

    Contiene las funciones de buscarMayor, buscarMenor, calcularPromedio y cuantosPares

---
+++ 03 Procesos/v3/procesamiento.c
@@ -0,0 +1,70 @@
+/**
+* @brief: procesamiento.c
+* @desc: Este archivo contiene las funciones que
+*		 realizan las operaciones de: búsqueda de
+*		 mayor y menor en el arreglo, promedio del
+*		 conjunto de datos y encontrar la cantidad
+*		 de números pares en el conjunto de datos
+**/
+
+#include "defs.h"
+
+int buscarMayor(int * datos)
+{
+	int mayor = datos[0];
+	register int i;
+
+	for (i = 1; i < N; i++)
+	{
+		if (datos[i] > mayor)
+		{
+			mayor = datos[i];
+		}
+	}
+
+	return mayor;
+}
+
+int buscarMenor(int * datos)
+{
+	int menor = datos[0];
+	register int i;
+
+	for (i = 1; i < N; i++)
+	{
+		if (datos[i] < menor)
+		{
+			menor = datos[i];
+		}
+	}
+
+	return menor;
+}
+
+int calcularPromedio(int * datos)
+{
+	long promedio = datos[0];
+	register int i;
+
+	for (i = 1; i < N; i++)
+	{
+		promedio += datos[i];
+	}
+
+	return (int)(promedio/N);
+}
+
+int cuantosPares(int * datos){
+	int nums_pares = 0;
+	register int i;
+
+	for (i = 0; i < N; i++)
+	{
+		if (datos[i]%2 == 0)
+		{
+			nums_pares++;
+		}
+	}
+
+	return nums_pares;
+}
