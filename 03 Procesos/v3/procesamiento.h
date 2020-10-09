4900f03bf32d4b83ada7dd2d7e8de6dedf431c2b
JesusDiaz08 <jesus_kdm@hotmail.com>
2020-10-09 15:08:26 -0500

    Contiene la definción de las funciones contenidas en el archivo procesamiento.c

---
+++ 03 Procesos/v3/procesamiento.h
@@ -0,0 +1,12 @@
+/**
+* @brief: procesamiento.h
+**/
+#ifndef PROCESAMIENTO_H
+#define PROCESAMIENTO_H
+
+int buscarMayor(int * datos);
+int buscarMenor(int * datos);
+int calcularPromedio(int * datos);
+int cuantosPares(int * datos);
+
+#endif
